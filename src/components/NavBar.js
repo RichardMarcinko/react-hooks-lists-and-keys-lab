db755f698bcebffcdc5ed91ea239b2bd36f6de81
John Coleman <jbcolemanjr@gmail.com>
2023-09-13 12:01:13 -0400

    complete list and keys lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,20 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((link) => {
+    return(
+    <a key={link} href={"#" + link}>{link}</a>
+    )
+  })
+  return <nav>{/* display an <a> tag for each link here */}
+      {linkElements}
+  </nav>;
+  
 }
 
 export default NavBar;
+
+
+// const colorElements = colors.map((color) => {
+//   return <li style={{ color: color }}>{color}</li>;
+// });
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,15 +1,23 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const projectTech = technologies.map((technology) => {
+    return(
+    <span key={technology}>{technology}</span>
+  )
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {projectTech}
       </div>
     </div>
   );
-}
+  
 
+
+}
 export default ProjectItem;
